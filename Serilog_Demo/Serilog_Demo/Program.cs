c10052248f4d6b8acea6f0b1abada496e4c4a2ee
Douglas <dglandre@gmail.com>
2023-01-08 17:57:57 -0300

    Program

--- Serilog_Demo/Serilog_Demo/Program.cs
+++ Serilog_Demo/Serilog_Demo/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Serilog_Demo
 {
@@ -13,11 +10,31 @@ namespace Serilog_Demo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json").Build();       // Lê as definições do arquivo appsettings.json
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config).CreateLogger(); /* O Usamos uma instancia de LoggerConfiguration e a partir das informações obtidas no 
+                                                                 * arquivo appsettings.json criamos um logger usando os sinks, os enrichers e as demais definições
+                                                                 */
+            try
+            {
+                Log.Information("API inicializando...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "A aplicação falhou a iniciar.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseSerilog()                                       // Define o Serilog como provedor de Log
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
